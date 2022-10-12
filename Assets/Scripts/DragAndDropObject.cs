28b3ff311b07e03f5eb9636e0ca73d2a005f4631
noelm <maeva.noel@edu.esiee.fr>
2022-10-12 17:41:46 +0200

    Drag and drop

---
+++ Assets/Scripts/DragAndDropObject.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DragAndDropObject : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Awake()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private Vector3 screenPoint;
+    private Vector3 offset;
+
+    void OnMouseDown()
+    {
+        screenPoint = Camera.main.WorldToScreenPoint(gameObject.transform.position);
+
+        offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
+
+    }
+
+    void OnMouseDrag()
+    {
+        Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
+
+        Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint) + offset;
+        curPosition.x = Mathf.Round(curPosition.x);
+        curPosition.y = Mathf.Round(curPosition.y);
+        curPosition.z = transform.position.z;
+        transform.position = curPosition;
+
+    }
+
+}
