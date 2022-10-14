d9df9f5ea791b740d98f65f7cdbbc02333033b47
Robin <robin.escaffre@edu.esiee.fr>
2022-10-14 12:23:21 +0200

    Finalv0.5

--- Assets/Scripts/DragAndDropObject.cs
+++ Assets/Scripts/DragAndDropObject.cs
@@ -8,6 +8,7 @@ public class DragAndDropObject : MonoBehaviour
     private Vector3 screenPoint;
     private Vector3 offset;
     public GameObject objet;
+    private List<Vector3> listPos = new List<Vector3>();
 
     public static GameObject CurrentlySelectedGameObject = null;
     public static bool objectOnDrag = false;
@@ -37,11 +38,25 @@ public class DragAndDropObject : MonoBehaviour
         curPosition.z = transform.position.z;
         transform.position = curPosition;
 
+        if (listPos.Count == 0)
+            listPos.Add(transform.position);
+        else
+            if (listPos[listPos.Count-1] != transform.position)
+                listPos.Add(transform.position);
+
+        if (listPos.Count == 3)
+        {
+            listPos[0] = listPos[1];
+            listPos[1] = listPos[2];
+            listPos.RemoveAt(2);
+        }
+        
+
         foreach (GameObject objet in GameObject.FindGameObjectsWithTag("object"))
         {
             if (transform.position.x == objet.transform.position.x && transform.position.y == objet.transform.position.y && objet != gameObject)
             {
-                curPosition.x = (Mathf.Round(curPosition.x*0.125f)/0.125f) + (gameObject.transform.localScale.x*8f);
+                curPosition = listPos[0];
                 gameObject.transform.position = curPosition;
             }
         }
--- Assets/Scripts/MoveCamera.cs
+++ Assets/Scripts/MoveCamera.cs
@@ -111,7 +111,7 @@ public class MoveCamera : MonoBehaviour
             if (Input.GetMouseButton(0))
             {
                 Vector3 difference = dragOrigin - cam.ScreenToWorldPoint(Input.mousePosition);
-                print("origin " + dragOrigin + " newPosition " + cam.ScreenToWorldPoint(Input.mousePosition) + " =difference" + difference);
+                //print("origin " + dragOrigin + " newPosition " + cam.ScreenToWorldPoint(Input.mousePosition) + " =difference" + difference);
                 //cam.transform.position += difference; //bez ograniczenia obszaru
 
                 cam.transform.position = ClampCamera(cam.transform.position + difference);   //ograniczenie obszaru
